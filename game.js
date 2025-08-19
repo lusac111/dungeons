100367602be1decf1b6cc4298b0b991a2789bc81
lusac111 <lusac11.1@gmail.com>
2025-08-19 20:34:20 +0000

    Added the score variable along with message arrays

---
+++ game.js
@@ -0,0 +1,9 @@
+// score variable - used to track the correct choices
+var playerScore;
+
+// survival messages array - used to track messages outputted when the player survives
+var survivalMessages = ["You survived!", "A rock fell from the ceiling... but it missed you", "You fell into a bottomless pit"];
+
+// death messages array - used to track messages outputted when the player dies
+var deathMessages = ["You died", "A brick fell on your head", "You ran into a bottomless pit... but you jumped over it"];
+
