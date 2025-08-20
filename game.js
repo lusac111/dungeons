8b7928d2e940253ef513710a0b3b4ec210483e94
lusac111 <lusac11.1@gmail.com>
2025-08-20 02:19:14 +0000

    used copilot to make the UI functional :)

--- game.js
+++ game.js
@@ -1,28 +1,67 @@
-// score variable - used to track the correct choices
-var playerScore;
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
 
-// survival messages array - used to track messages outputted when the player survives
-var survivalMessages = ["You survived!", "A rock fell from the ceiling... but it missed you", "You fell into a bottomless pit"];
+const deathMessages = [
+    'You fell into a pit of spikes!',
+    'A shadowy figure ended your journey!',
+    'You were devoured by a lurking beast!',
+    'A trapdoor opened beneath you!',
+    'You were struck by a deadly curse!'
+]
 
-// death messages array - used to track messages outputted when the player dies
-var deathMessages = ["You died", "A brick fell on your head", "You ran into a bottomless pit... but you jumped over it"];
+const survivalMessages = [
+    'You narrowly avoid danger and press on.',
+    'A hidden passage keeps you safe.',
+    'You hear ominous sounds but escape unharmed.',
+    'You find a torch and light your way.',
+    'You dodge a falling rock just in time.'
+]
 
-//function to break console text by 3 rows
-function textBreak() {
-console.log("");
-console.log("");
-console.log("");
+function startGame() {
+    showDirectionChoice()
 }
 
-// welcome text
-console.log("WELCOME TO THE");
-console.log("______ _   _ _   _ _____  _____ _____ _   _") ;
-console.log("|  _  \ | | | \ | |  __ \|  ___|  _  | \ | |");
-console.log("| | | | | | |  \| | |  \/| |__ | | | |  \| |");
-console.log("| | | | | | | . ` | | __ |  __|| | | | . ` |");
-console.log("| |/ /| |_| | |\  | |_\ \| |___\ \_/ / |\  |");
-console.log("|___/  \___/\_| \_/\____/\____/ \___/\_| \_/");
-textBreak();
+function showDirectionChoice() {
+    textElement.innerText = 'Choose a direction to travel'
+    optionButtonsElement.innerHTML = ''
+    const directions = ['Forward', 'Backward', 'Left', 'Right']
+    directions.forEach(dir => {
+        const button = document.createElement('button')
+        button.innerText = dir
+        button.classList.add('btn')
+        button.addEventListener('click', () => handleDirection(dir))
+        optionButtonsElement.appendChild(button)
+    })
+}
 
-// game logic
+function handleDirection(direction) {
+    if (Math.random() < 0.3) {
+        showDeath()
+    } else {
+        showSurvival(direction)
+    }
+}
 
+function showDeath() {
+    const msg = deathMessages[Math.floor(Math.random() * deathMessages.length)]
+    textElement.innerText = msg
+    optionButtonsElement.innerHTML = ''
+    const restartBtn = document.createElement('button')
+    restartBtn.innerText = 'Restart'
+    restartBtn.classList.add('btn')
+    restartBtn.addEventListener('click', startGame)
+    optionButtonsElement.appendChild(restartBtn)
+}
+
+function showSurvival(direction) {
+    const msg = survivalMessages[Math.floor(Math.random() * survivalMessages.length)]
+    textElement.innerText = `${msg} You travel ${direction.toLowerCase()}.`
+    optionButtonsElement.innerHTML = ''
+    const continueBtn = document.createElement('button')
+    continueBtn.innerText = 'Continue'
+    continueBtn.classList.add('btn')
+    continueBtn.addEventListener('click', showDirectionChoice)
+    optionButtonsElement.appendChild(continueBtn)
+}
+
+startGame()
